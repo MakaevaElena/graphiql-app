8c8c03f10afdb3d6dde49de93c70db68bc855b99
Makaeva Elena <yamaslena@yandex.ru>
2023-12-12 16:10:56 +0700

    feat: change color for  RequestEditor

--- src/Components/RequestEditor/RequestEditor.tsx
+++ src/Components/RequestEditor/RequestEditor.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
 import { Box, Typography } from '@mui/material';
 import EditorJS from '@editorjs/editorjs';
+import { wrappwerTextEditor } from './styles';
+// import Header from '@editorjs/header';
+// import List from '@editorjs/list';
 
 const RequestEditor: React.FC = () => {
   const editor = new EditorJS({
     holder: 'editor',
     minHeight: 0,
+    // tools: {
+    //   header: {
+    //     class: Header,
+    //     inlineToolbar: ['link'],
+    //   },
+    //   list: {
+    //     class: List,
+    //     inlineToolbar: true,
+    //   },
+    // },
   });
 
   console.log(editor);
@@ -13,7 +26,7 @@ const RequestEditor: React.FC = () => {
   return (
     <Box>
       <Typography variant="h4">RequestEditor</Typography>
-      <Box id="editor"></Box>
+      <Box sx={wrappwerTextEditor} id="editor"></Box>
     </Box>
   );
 };
