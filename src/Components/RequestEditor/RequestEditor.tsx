cf46717bdb5cbbb36729d9017f2e745281cff49b
Makaeva Elena <yamaslena@yandex.ru>
2023-12-12 16:01:28 +0700

    feat: add Editor.js to RequestEditor

--- src/Components/RequestEditor/RequestEditor.tsx
+++ src/Components/RequestEditor/RequestEditor.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
 import { Box, Typography } from '@mui/material';
+import EditorJS from '@editorjs/editorjs';
 
 const RequestEditor: React.FC = () => {
+  const editor = new EditorJS({
+    holder: 'editor',
+    minHeight: 0,
+  });
+
+  console.log(editor);
+
   return (
     <Box>
       <Typography variant="h4">RequestEditor</Typography>
+      <Box id="editor"></Box>
     </Box>
   );
 };
