d6a3b901bc9b1c9ecd5d255de5b537d36580268c
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-12-17 22:03:45 +0200

    feat: update editor page

--- src/pages/EditorPage/EditorPage.tsx
+++ src/pages/EditorPage/EditorPage.tsx
@@ -9,18 +9,16 @@ import {
   // wrapperDocumentation,
   wrapperEndpoint,
   wrapperGraphQL,
-  wrapperHeadersEditor,
+  wrapperHelpersEditor,
   wrapperRequestEditor,
   wrapperResponseSection,
-  wrapperVariablesEditor,
 } from './styles';
 import { setDocsIsOpen } from '../../store/slices/UISlice';
 import Documentation from '../../Components/Documentation/Documentation';
-import HeadersEditor from '../../Components/HeadersEditor/HeadersEditor';
-import VariablesEditor from '../../Components/VariablesEditor/VariablesEditor';
 import ResponseSection from '../../Components/ResponseSection/ResponseSection';
 import RequestEditor from '../../Components/RequestEditor/RequestEditor';
 import Endpoint from '../../Components/Endpoint/Endpoint';
+import TabsPanel from '../../Components/Tabs/TabsPanel';
 
 // const currentPath = location.pathname;
 
@@ -52,11 +50,8 @@ const EditorPage: React.FC = () => {
         <Box sx={wrapperResponseSection}>
           <ResponseSection />
         </Box>
-        <Box sx={wrapperVariablesEditor}>
-          <VariablesEditor />
-        </Box>
-        <Box sx={wrapperHeadersEditor}>
-          <HeadersEditor />
+        <Box sx={wrapperHelpersEditor}>
+          <TabsPanel />
         </Box>
         <Box sx={wrapperDocumentation}>{docsIsOpen && <Documentation />}</Box>
       </Box>
